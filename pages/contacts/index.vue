03682a73b4054300492438a72a5bc11d98e27f73
Ivanov Egor <frontninja@bk.ru>
2018-04-29 03:27:58 +0300

    form test #3

--- pages/contacts/index.vue
+++ pages/contacts/index.vue
@@ -1,12 +1,20 @@
 <template>
   <div>
       <h1>Контакты</h1>
-      <form name="contact" method="POST" netlify >
-        <input type="text" placeholder="Тема">
-        <input type="text" placeholder="Номер телефона">
-        <input type="textfield" placeholder="Сообщение">
-        <button type="submit">Отправить</button>
-      </form>
+<form name="contact" method="POST" netlify>
+  <p>
+    <label>Ваше имя: <input type="text" name="name"></label>   
+  </p>
+  <p>
+    <label>Ваш Email: <input type="email" name="email"></label>
+  </p>
+  <p>
+    <label>Сообщение: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
   </div>
 </template>
 <script>
