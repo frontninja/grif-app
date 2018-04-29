8c8f15f92ec70f7272664d80adbb4902356884c5
Ivanov Egor <frontninja@bk.ru>
2018-04-29 03:26:37 +0300

    form test

--- pages/contacts/index.vue
+++ pages/contacts/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
       <h1>Контакты</h1>
-      <form >
+      <form name="contact" method="POST" netlify >
         <input type="text" placeholder="Тема">
         <input type="text" placeholder="Номер телефона">
         <input type="textfield" placeholder="Сообщение">
